e781fa51b07c51698968c92e7b846cf92706722f
Асхат Баймуканов <askhatbaimukanov@Ashats-MacBook-Air.local>
2022-03-25 10:56:53 +0500

    Start new project TodoGo

---
+++ TodoGo/Controllers/TodoListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoGo
+//
+//  Created by Асхат Баймуканов on 25.03.2022.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
